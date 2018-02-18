c01ce962b16beea215b51575abd59e5798f1504b
Guillaume Marc <marcguillaume77@gmail.com>
2018-02-18 13:39:13 +0100

    first push

---
+++ exos/exo6.php
@@ -0,0 +1,40 @@
+<?php
+require_once '../inc/functions.php';
+
+/*
+ * Exo 6 : Mamie reçoit les p’tits enfants
+ *
+ * Bon, tant qu'on y est, ça serait bien aussi de calculer leur âge.
+ * J'ai toujours eu la bonne mémoire des dates, je connais leur
+ * date de naissance par coeur.
+ *
+ * Seulement, pour calculer leur âge à partir de leur date de naissance,
+ * saperlipopette !
+ *
+ * Si je te rajoute un paramètre `birth` à l'URL, tu crois que tu pourrais
+ * aussi me rajouter l'âge ? Je te passe la date sous la forme DD-MM-YYYY.
+ * Par exemple :
+ *   `exo5.php?name=Zelda&birth=18-05-2003`
+ *
+ * Ensuite, il te "suffirait" qu'à récupérer, découper, puis comparer…
+ *   http://php.net/manual/fr/function.explode.php
+ *   http://php.net/manual/fr/function.date.php
+ *
+ *
+ * Allez, bon courage… (mouhahaha)
+ */
+
+function hello() {
+
+}
+
+
+
+
+/*
+ * Tests
+ * Pas touche !
+ */
+displayExo(6, (
+    hello() === 'Bonjour Elisa, comment tu vas ? Tu as 15 ans maintenant !'
+));
